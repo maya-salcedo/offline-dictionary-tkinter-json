64be8e13dce88bf3a0665f7ba9ced2a9a99aa10a
maya <maya.salcedo07@gmail.com>
2020-10-23 12:13:32 +0300

    horizontal and vertical scroll added and some more

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ data = json.load(open('data.json'))
 def definition(word):
     if word in data:
         return(data[word])
+    elif word.lower() in data:
+        return (data[word.lower()])
     elif word.title() in data:
-        return data[word.title()]
+        return(data[word.title()])
     elif word.upper() in data:
-        return data[word.upper()]
+        return(data[word.upper()])
     else:
         try:
-            alternative_word = difflib.get_close_matches(word, data, n=1)
-            word = alternative_word[0]
-            return("Do you mean " + word + "?\n" + data[word])
-        except IndexError:
-            return('that is not in my dictionary. sorry')
+            pass
+        except IndexError or TypeError:
+            return ['that is not in my dictionary. sorry']
 
 def view_definition():
     list1.delete(0, END)
@@ -41,19 +41,21 @@ b1 = Button(window, text="Search", width=12, command=view_definition)
 b1.grid(row=1, column=5)
 b2 = Button(window, text="Clear", width=12, command=delete)
 b2.grid(row=1, column=6)
+
 list1 = Listbox(window, height=6, width=55)
 list1.grid(row=2, column=1, rowspan=6, columnspan=7)
 
-sb1 = Scrollbar(window)
-sb1.grid(row=2, column=7, rowspan=5)
+sby = Scrollbar(window)
+sby.grid(row=2, column=7, rowspan=5)
 
-#sb2 = Scrollbar(window)
-#sb2.grid(row=7, column= 1, columnspan=5)
+sbx = Scrollbar(window, orient=HORIZONTAL)
+sbx.grid(row=9, column=2, columnspan=4)
 
-list1.configure(yscrollcommand=sb1.set)
-#list1.configure(xscrollcommand=sb2.set)
-sb1.configure(command=list1.yview)
-#sb2.configure(command=list1.xview)
+
+list1.configure(yscrollcommand=sby.set, xscrollcommand=sbx.set)
+
+sby.configure(command=list1.yview)
+sbx.configure(command=list1.xview)
 
 window.mainloop()
 
